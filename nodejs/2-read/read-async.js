8bf719f808e27cac9ce892f2feeccdbfce39388c
Markus Re1 <markus@re1.at>
2017-12-20 13:16:31 +0100

    Add submodules

--- nodejs/2-read/read-async.js
+++
@@ -1,8 +0,0 @@
-fs = require('fs')
-
-console.time("read-async")
-files = fs.readdir('../', 'utf8', function (err, file) {
-    // Log error or file
-    err ? console.log(err) : console.log(file)
-})
-console.timeEnd("read-async")
--- nodejs/2-read/read-sync.js
+++
@@ -1,6 +0,0 @@
-fs = require('fs')
-
-console.time("read-sync")
-let files = fs.readdirSync('../', 'utf8')
-console.log(files)
-console.timeEnd("read-sync")
